21ce9eae31adf82220aca090b0d982afb5373825
Jesse Griffin <jesse.griffin@gmail.com>
2020-01-28 02:41:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,12 +1,13 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  companies = ["Knight", "Swift", "Abilene", "Barr-Nunn"]
 end
 
 def add_element_to_end_of_array(array, element)
+  ice_cream = ["Chocolate", "Vanilla", "Mint", "Stawberry"]
   
 end
 
@@ -15,11 +16,13 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  friends = ["Evan", "Scott", "Phil", "TJ"]
+  friends.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  friends = ["Jon", "Kevin", "Gio"]
+  friends.shift
 end
 
 def retrieve_element_from_index(array, index_number)
